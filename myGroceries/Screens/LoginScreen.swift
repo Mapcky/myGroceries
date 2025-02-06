5993f9e8c3ace6a5c5e0df6e969e83200e4e281f
Mapcky <mperano625@gmail.com>
2025-02-06 09:47:22 -0300

    loginScreen

---
+++ myGroceries/Screens/LoginScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LoginScreen.swift
+//  myGroceries
+//
+//  Created by Mateo Andres Perano on 06/02/2025.
+//
+
+import SwiftUI
+
+struct LoginScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginScreen()
+}
