372a840f7cdafd59db5a688aba0dabe7a3bae2f4
Mapcky <mperano625@gmail.com>
2025-02-06 15:08:23 -0300

    Tabs & login

---
+++ myGroceries/Screens/HomeScreen.swift
@@ -0,0 +1,67 @@
+//
+//  HomeScreen.swift
+//  myGroceries
+//
+//  Created by Mateo Andres Perano on 06/02/2025.
+//
+
+import SwiftUI
+
+enum AppScreen: Hashable, Identifiable, CaseIterable {
+    case home
+    case myProducts
+    case cart
+    case profile
+    
+    var id: AppScreen { self }
+}
+
+extension AppScreen {
+    
+    @ViewBuilder
+    var label: some View {
+        switch self {
+        case .home:
+            Label("Home", systemImage: "heart.fill")
+        case .myProducts:
+            Label("My Products", systemImage: "star")
+        case .cart:
+            Label("Cart", systemImage: "cart")
+        case .profile:
+            Label("Profile", systemImage: "person.fill")
+        }
+    }
+    
+    @ViewBuilder
+    var destination: some View {
+        switch self {
+        case .home:
+            Text("Home")
+        case .myProducts:
+            Text("My Products")
+        case .cart:
+            Text("Cart")
+        case .profile:
+            Text("Profile")
+        }
+    }
+    
+}
+
+struct HomeScreen: View {
+    
+    @State var selection: AppScreen?
+    var body: some View {
+        TabView(selection: $selection) {
+            ForEach(AppScreen.allCases) { screen in
+                screen.destination
+                    .tag(screen as AppScreen?)
+                    .tabItem {screen.label}
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
