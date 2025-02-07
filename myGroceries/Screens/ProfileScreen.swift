b8aa3ca4f96ff123d19fa51d92cae2a654fd0690
Mapcky <mperano625@gmail.com>
2025-02-07 08:23:43 -0300

    Authentication

---
+++ myGroceries/Screens/ProfileScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileScreen.swift
+//  myGroceries
+//
+//  Created by Mateo Andres Perano on 06/02/2025.
+//
+
+import SwiftUI
+
+struct ProfileScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileScreen()
+}
---
+++ myGroceries/View Modifiers/RequiresAuthentication.swift
@@ -0,0 +1,8 @@
+//
+//  RequiresAuthentication.swift
+//  myGroceries
+//
+//  Created by Mateo Andres Perano on 06/02/2025.
+//
+
+import Foundation
