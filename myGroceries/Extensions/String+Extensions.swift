efd29fa88f6199e2eb87b80df93742b5731a00f5
Mapcky <mperano625@gmail.com>
2025-02-20 09:26:13 -0300

    profile fields

--- myGroceries/Extensions/String+Extensions.swift
+++ myGroceries/Extensions/String+Extensions.swift
@@ -12,4 +12,10 @@ extension String {
     var isEmptyOrWhiteSpaces: Bool {
         self.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
     }
+    
+    var isZipCode: Bool {
+        // Adjust this regex for your ZIP code requirements (US format example here)
+        let zipCodeRegex = "^[0-9]{5}(-[0-9]{4})?$"
+        return NSPredicate(format: "SELF MATCHES %@", zipCodeRegex).evaluate(with: self)
+    }
 }
